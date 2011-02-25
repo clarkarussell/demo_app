a5951dd6dc382b1f5a50ff60ed390f89d9cb744c
Clark Russell <clarkarussell@gmail.com>
2011-02-24 19:48:51 -0800

    Simplified the Users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   # GET /users/new
